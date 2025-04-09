e0f0713bd335b4cfacbf8c5c4be476b785da7cbd
SkrobolTymek <Tymoteusz.skrobol@gmail.com>
2025-04-09 21:07:50 +0200

    od nowa

---
+++ target/debug/deps/ferdexc-bf14d438a0f8f648.d
@@ -0,0 +1,5 @@
+/home/saizzan/programowanie/ferdexc/target/debug/deps/libferdexc-bf14d438a0f8f648.rmeta: src/main.rs
+
+/home/saizzan/programowanie/ferdexc/target/debug/deps/ferdexc-bf14d438a0f8f648.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ferdexc-fcbf47ca720c5423.d
@@ -0,0 +1,5 @@
+/home/saizzan/programowanie/ferdexc/target/debug/deps/libferdexc-fcbf47ca720c5423.rmeta: src/main.rs
+
+/home/saizzan/programowanie/ferdexc/target/debug/deps/ferdexc-fcbf47ca720c5423.d: src/main.rs
+
+src/main.rs:
